fa506c83932ed39a8fe194f03c514d33ada065b2
José Augusto Salim <jasalim@unicamp.br>
2024-01-19 12:32:20 -0300

    Added translations

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@
 # Retrieves and updates eml.tsv distribution
 #
 
-curl -L "https://docs.google.com/spreadsheets/d/1pi0j3xTtFaWHlUO-IjcB0kSQxA45Ew0tnk5v7-_ygH0/export?format=tsv" >> dist/eml.tsv
+curl -L "https://docs.google.com/spreadsheets/d/1pi0j3xTtFaWHlUO-IjcB0kSQxA45Ew0tnk5v7-_ygH0/export?format=tsv" > dist/eml.tsv
